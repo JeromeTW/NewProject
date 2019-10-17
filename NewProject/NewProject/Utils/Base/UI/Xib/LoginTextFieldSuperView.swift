c7bc3fa0ee222a41ae85d7432c9f743937b006bc
Jerome Hsieh <jerome.developer.tw@gmail.com>
2019-10-17 16:21:24 +0800

    添加 Xib with Storyboard 的使用。

---
+++ NewProject/NewProject/Utils/Base/UI/Xib/LoginTextFieldSuperView.swift
@@ -0,0 +1,30 @@
+//
+//  LoginTextFieldSuperView.swift
+//  VCLifeCycle
+//
+//  Created by JEROME on 2019/10/17.
+//  Copyright © 2019 jerome. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class LoginTextFieldSuperView: UIView {
+  
+  override func prepareForInterfaceBuilder() {
+    super.prepareForInterfaceBuilder()
+    addXibView()
+  }
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    addXibView()
+  }
+  
+  func addXibView() {
+    if let loginTextFIeldView = Bundle(for: LoginTextFieldView.self).loadNibNamed(LoginTextFieldView.className, owner: nil, options: nil)?.first as? UIView {
+      addSubview(loginTextFIeldView)
+      loginTextFIeldView.frame = bounds
+    }
+  }
+  
+}
---
+++ NewProject/NewProject/Utils/Base/UI/Xib/LoginTextFieldView.swift
@@ -0,0 +1,15 @@
+//
+//  LoginTextFieldView.swift
+//  VCLifeCycle
+//
+//  Created by JEROME on 2019/10/17.
+//  Copyright © 2019 jerome. All rights reserved.
+//
+
+import UIKit
+
+class LoginTextFieldView: BaseView {
+  @IBOutlet weak var splitLineView: UIView!
+  @IBOutlet weak var textField: UITextField!
+  @IBOutlet weak var label: UILabel!
+}
