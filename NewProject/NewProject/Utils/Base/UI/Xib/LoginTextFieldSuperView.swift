83262b4de2c6220d5b1c2c2077a5168a014a45cf
Jerome Hsieh <jerome.developer.tw@gmail.com>
2019-10-18 08:04:23 +0800

    LoginTextFieldSuperView 繼承 BaseView

--- NewProject/NewProject/Utils/Base/UI/Xib/LoginTextFieldSuperView.swift
+++ NewProject/NewProject/Utils/Base/UI/Xib/LoginTextFieldSuperView.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-@IBDesignable class LoginTextFieldSuperView: UIView {
+@IBDesignable class LoginTextFieldSuperView: BaseView {
   
   override func prepareForInterfaceBuilder() {
     super.prepareForInterfaceBuilder()
