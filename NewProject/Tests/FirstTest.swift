d5eb43c1b86f0e2696c327f8a45f620d0d46c25f
Jerome Hsieh <jerome.developer.tw@gmail.com>
2019-11-28 15:05:56 +0800

    1. 新增 FirstTest.swift. 2.設定好 Test Target 的 Other Swift Flag, 在 Test Target 也可以 Run #if DEBUG 的程式。

---
+++ NewProject/Tests/FirstTest.swift
@@ -0,0 +1,35 @@
+//
+//  FirstTest.swift
+//  Tests
+//
+//  Created by JEROME on 2019/11/28.
+//  Copyright © 2019 jerome. All rights reserved.
+//
+
+import XCTest
+import HouJia
+
+class FirstTest: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+      logF("First Test~")
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
